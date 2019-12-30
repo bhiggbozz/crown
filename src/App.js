18543e674182ebe157cff62f891e18ef78d0eb2a
bhiggbozz <Sunday.Omoyele@ISW-ENGR-SO2>
2019-12-30 15:44:56 +0100

    directory building

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import HomePage from "./homepage.component.jsx";
+import HomePage from "./pages/homepage/homepage.component.jsx";
 
 function App() {
   return (
