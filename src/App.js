5be127eaa1953b800491f8c6e9242f1657431349
bhiggbozz <Sunday.Omoyele@ISW-ENGR-SO2>
2020-01-31 16:19:24 +0100

    new changes

--- src/App.js
+++ src/App.js
@@ -2,11 +2,41 @@ import React from "react";
 import logo from "./logo.svg";
 import "./App.css";
 import HomePage from "./pages/homepage/homepage.component.jsx";
+import ShopPage from "./pages/shop/shop.component.jsx";
+import { Switch, Route } from "react-router-dom";
+import Header from "./components/header/header.component.jsx";
+
+const HatsPage = () => (
+  <div>
+    <h1> HATS PAGE</h1>
+  </div>
+);
+const TopicsList = () => {
+  return (
+    <div>
+      <h1>TOPIC DETAIL PAGE</h1>
+    </div>
+  );
+};
+
+const TopicDetail = props => (
+  <div>
+    <button onclick={() => props.history.push("/hats")}> Hats </button>
+    <h1>TOPIC LIST PAGE</h1>
+  </div>
+);
 
 function App() {
   return (
     <div>
-      <HomePage />
+      <Header />
+      <Switch>
+        <Route exact path="/" component={HomePage} />
+        <Route exact path="/shop" component={ShopPage} />
+
+        <Route exact path="/topics/:topicId" component={TopicDetail} />
+        <Route exact path="/topicslist" component={TopicsList} />
+      </Switch>
     </div>
   );
 }
--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -42,7 +42,7 @@ class Directory extends React.Component {
     return (
       <div className="directory-menu">
         {this.state.sections.map(({ title, imageUrl, id, size }) => (
-          <MenuItem key={id} title={title} imageUrl={imageUrl} />
+          <MenuItem key={id} title={title} imageUrl={imageUrl} size={size} />
         ))}
       </div>
     );
